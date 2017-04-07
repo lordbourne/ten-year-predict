ff4d4dd6cdb40f8776d56dd3b9db39ac8fd11cc8
lordbourne <1060697802@qq.com>
2017-04-08 02:05:20 +0800

    fix bugs of two input detection functions

--- js/prod-input.js
+++ js/prod-input.js
@@ -1,88 +1,51 @@
 $(document).ready(function() {
   // 如果曾经输入过表单，就直接利用本地存储方式填充表单
   $('#data-input input').each(function(index, el) {
-    $(this).val(localStorage.getItem($(this)[0].name))
+    $(this).val(localStorage.getItem($(this)[0].name));
   });
 
   // 错误检测函数
-  // var checkInput = function() {
-  //   var b = /^[1-9]*[1-9][0-9]*$/;
-  //   $('#data-input input').each(function(index, el) {
-  //     $(this).blur(function(event) {
-  //       var tmp = $(this).val();
-  //       if (index == 0) {
-  //         if (!b.test(tmp) || parseInt(tmp) < 1 || parseInt(tmp) > 20) {
-  //           $(this).val("").focus();
-  //           alert("月实动人均件数必须是正整数且在数值区间内！");
-  //           return false;
-  //         }
-  //       } else if (index == 1) {
-  //         if (!b.test(tmp) || parseInt(tmp) < 1000 || parseInt(tmp) > 10000) {
-  //           $(this).val("").focus();
-  //           alert("件均保费必须是正整数且在数值区间内！");
-  //           return false;
-  //         }
-  //       } else if (index == 2) {
-  //         if (!b.test(tmp) || parseInt(tmp) != 25) {
-  //           $(this).val("").focus();
-  //           alert("佣金率必须是正整数且在数值区间内！");
-  //           return false;
-  //         }
-  //       } else if (index == 3) {
-  //         if (!b.test(tmp) || parseInt(tmp) != 70) {
-  //           $(this).val("").focus();
-  //           alert("开单率必须是正整数且在数值区间内！");
-  //           return false;
-  //         }
-  //       } else if (index == 4) {
-  //         if (!b.test(tmp) || parseInt(tmp) < 1000 || parseInt(tmp) > 10000000) {
-  //           $(this).val("").focus();
-  //           alert("个人月首佣必须是正整数且在数值区间内！");
-  //           return false;
-  //         }
-  //       }
-  //       return true;
-  //     });
-  //   });
-  // }
-  function checkInputAgain() {
-    // var b = /^[1-9]*[1-9][0-9]*$/;
-    // $('#data-input input').each(function(index, el) {
-    //   var tmp = $(this).val();
-    //   if (index == 0) {
-    //     if (!b.test(tmp) || parseInt(tmp) < 1 || parseInt(tmp) > 20) {
-    //       $(this).val("").focus();
-    //       alert("月实动人均件数必须是正整数且在数值区间内！");
-    //       return false;
-    //     }
-    //   } else if (index == 1) {
-    //     if (!b.test(tmp) || parseInt(tmp) < 1000 || parseInt(tmp) > 10000) {
-    //       $(this).val("").focus();
-    //       alert("件均保费必须是正整数且在数值区间内！");
-    //       return false;
-    //     }
-    //   } else if (index == 2) {
-    //     if (!b.test(tmp) || parseInt(tmp) != 25) {
-    //       $(this).val("").focus();
-    //       alert("佣金率必须是正整数且在数值区间内！");
-    //       return false;
-    //     }
-    //   } else if (index == 3) {
-    //     if (!b.test(tmp) || parseInt(tmp) != 70) {
-    //       $(this).val("").focus();
-    //       alert("开单率必须是正整数且在数值区间内！");
-    //       return false;
-    //     }
-    //   } else if (index == 4) {
-    //     if (!b.test(tmp) || parseInt(tmp) < 1000 || parseInt(tmp) > 10000000) {
-    //       $(this).val("").focus();
-    //       alert("个人月首佣必须是正整数且在数值区间内！");
-    //       return false;
-    //     }
-    //   }
-    //   return true;
-    // });
-    //
+  var checkInput = function () {
+    var b = /^[1-9]*[1-9][0-9]*$/;
+    $('#data-input input').each(function(index, el) {
+      $(this).blur(function(event) {
+        var tmp = $(this).val();
+        if (index == 0) {
+          if (!b.test(tmp) || parseInt(tmp) < 1 || parseInt(tmp) > 20) {
+            $(this).val("").focus();
+            alert("月实动人均件数必须是正整数且在数值区间内！");
+            return false;
+          }
+        } else if (index == 1) {
+          if (!b.test(tmp) || parseInt(tmp) < 1000 || parseInt(tmp) > 10000) {
+            $(this).val("").focus();
+            alert("件均保费必须是正整数且在数值区间内！");
+            return false;
+          }
+        } else if (index == 2) {
+          if (!b.test(tmp) || parseInt(tmp) != 25) {
+            $(this).val("").focus();
+            alert("佣金率必须是正整数且在数值区间内！");
+            return false;
+          }
+        } else if (index == 3) {
+          if (!b.test(tmp) || parseInt(tmp) != 70) {
+            $(this).val("").focus();
+            alert("开单率必须是正整数且在数值区间内！");
+            return false;
+          }
+        } else if (index == 4) {
+          if (!b.test(tmp) || parseInt(tmp) < 1000 || parseInt(tmp) > 10000000) {
+            $(this).val("").focus();
+            alert("个人月首佣必须是正整数且在数值区间内！");
+            return false;
+          }
+        }
+        return true;
+      });
+    });
+  };
+  var checkInputAgain = function () {
     var b = /^[1-9]*[1-9][0-9]*$/;
     var tmp = "";
     tmp = $('#data-input input').filter(':eq(0)').val();
@@ -116,11 +79,46 @@ $(document).ready(function() {
       return false;
     }
     return true;
-  }
-
-
-
-  // checkInput();
+  };
+  // var checkInputAgain = function () {
+  //   var b = /^[1-9]*[1-9][0-9]*$/;
+  //   $('#data-input input').each(function(index, el) {
+  //     var tmp = $(this).val();
+  //     if (index == 0) {
+  //       if (!b.test(tmp) || parseInt(tmp) < 1 || parseInt(tmp) > 20) {
+  //         $(this).val("").focus();
+  //         alert("月实动人均件数必须是正整数且在数值区间内！");
+  //         return false;
+  //       }
+  //     } else if (index == 1) {
+  //       if (!b.test(tmp) || parseInt(tmp) < 1000 || parseInt(tmp) > 10000) {
+  //         $(this).val("").focus();
+  //         alert("件均保费必须是正整数且在数值区间内！");
+  //         return false;
+  //       }
+  //     } else if (index == 2) {
+  //       if (!b.test(tmp) || parseInt(tmp) != 25) {
+  //         $(this).val("").focus();
+  //         alert("佣金率必须是正整数且在数值区间内！");
+  //         return false;
+  //       }
+  //     } else if (index == 3) {
+  //       if (!b.test(tmp) || parseInt(tmp) != 70) {
+  //         $(this).val("").focus();
+  //         alert("开单率必须是正整数且在数值区间内！");
+  //         return false;
+  //       }
+  //     } else if (index == 4) {
+  //       if (!b.test(tmp) || parseInt(tmp) < 1000 || parseInt(tmp) > 10000000) {
+  //         $(this).val("").focus();
+  //         alert("个人月首佣必须是正整数且在数值区间内！");
+  //         return false;
+  //       }
+  //     }
+  //     return true;
+  //   });
+  // };
+  checkInput();
   $('#setdata').click(function(event) {
     event.preventDefault();
     // console.log(checkInputAgain());
