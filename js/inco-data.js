ea49689de17f57560bc714c8700f6fdc9caff1b9
lordbourne <l060697802@qq.com>
2017-05-04 09:48:42 +0800

    fix some bugs

--- js/inco-data.js
+++ js/inco-data.js
@@ -2,8 +2,6 @@
 $(document).ready(function() {
 
   var agntnum = localStorage.agntnum;
-  // 没缓存或者换人了都要更新缓存
-  // if (typeof localStorage.agntname == "undefined") {
     $.ajax({
       // url: 'http://10.12.1.88/Query/servlet/com.base.JsonDataServlet',
       url: 'http://taikangsc.com:9080/bip/servlet/com.base.JsonDataServlet',
@@ -14,12 +12,18 @@ $(document).ready(function() {
       data: {
         agntnum: agntnum
       },
-      beforeSend:function(XMLHttpRequest){
+      beforeSend: function(XMLHttpRequest){
         $('#page').hide();
-        $('body').append('<div class="spinner" id="spinner"></div>'); //在后台返回success之前显示loading图标
+        // $('body').append('<div class="spinner" id="spinner"></div>'); //在后台返回success之前显示loading图标
+        $('#spinner').show();
       },
       success: function(data) {
         console.log(data);
+        // 未请求到数据
+        if (!data) {
+          alert('未请求到数据');
+          window.location.href = "inco-input.html";
+        }
         // 获取模板并加载数据
         $.ajax({
           url: 'tpl/tpl-inco-data.html',
@@ -40,41 +44,21 @@ $(document).ready(function() {
             $('#page').html(html);
           }
         });
-        $('#spinner').remove();
+        $('#spinner').hide();
         $('#page').slideDown();
         // 存储数据
         for (key in data) {
           localStorage.setItem(key, data[key]);
         }
+      },
+      error: function (data) {
+        alert('error');
       }
     });
-  // } else {
-  //   // 信息先从本地提取,本地有就不用请求了
-  //   $('header h1 span').html((localStorage.nsr/10000).toFixed(2));
-  //   data = [
-  //     'agntnum',// 工号
-  //     'agntname',// 姓名
-  //     'agtype',// 职级
-  //     'dteapp',// 入司时间
-  //     'zxzrs',// 直辖组人力
-  //     'zxbrs',// 直辖部人力
-  //     'sxrs',// 所辖人力
-  //     'zxas',// 直辖 AS
-  //     'zxss',// 直辖 SS
-  //     'zxum',// 直辖 UM
-  //     'zxad'// 直辖 UM
-  //   ];
-  //   $spans = $('section span');
-  //   for (index in data) {
-  //     // console.log(data[key]);
-  //     key = data[index];
-  //     $spans.filter('[data-' + key + ']').html(localStorage[key]);
-  //   }
-  // }
-
   $('#followme').click(function(event) {
     event.preventDefault();
     gotoNextPage(this);
   });
 });
+
 
